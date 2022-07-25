f742fe1fedb26c0c34a047adc2912d21c6e013e3
alle@alessandro-pifferi.com <alle@alessandro-pifferi.com>
2022-07-25 21:29:14 +0200

    Ristorantee Function Componet

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+// component navbar 
+function Navbar() {
+    return (
+        <nav className="navbar bg-dark">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1" id="navbar__title">Ristorante Cinese</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
